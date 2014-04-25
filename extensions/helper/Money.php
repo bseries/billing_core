002d9d12f7cb7bb99bc1749ac9d44abc7112a03b
David Persson <davidpersson@gmx.de>
2014-04-25 17:09:53 +0200

    Markup in money format.

--- extensions/helper/Money.php
+++ extensions/helper/Money.php
@@ -19,14 +19,20 @@ class Money extends \lithium\template\Helper {
 
 	public function format($value, $type = null, array $options = []) {
 		$options += [
-			'locale' => null
+			'locale' => null,
+			'html' => false
 		];
 		$locale = $options['locale'] ?: $this->_locale();
 
 		switch ($type) {
 			case 'money':
 				$formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
-				return $formatter->formatCurrency($value->getAmount() / 100, $value->getCurrency());
+				$result = $formatter->formatCurrency($value->getAmount() / 100, $value->getCurrency());
+
+				if ($options['html']) {
+					return $this->_applyMarkup($result);
+				}
+				return $result;
 			case 'decimal':
 				$formatter = new NumberFormatter($locale, NumberFormatter::DECIMAL);
 				$formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, 2);
@@ -37,6 +43,40 @@ class Money extends \lithium\template\Helper {
 				}
 				return $formatter->format($value / 100);
 		}
+	}
+
+	// @todo Make this work with formats without currency symbols
+	// as well as ,/. separators. Use strrpos.
+	protected function _applyMarkup($string) {
+		$before = mb_substr($string, 0, -4);
+		$places = mb_substr($string, -4, 2);
+		$after = mb_substr($string, -2, 2);
+
+		return $before . '<span class="decimal-places">' . $places . '</span>' . '<span class="currency-symbol">' . $after . '</span>';
+	}
+
+	protected function _superscriptPlaces($string) {
+		$map = [
+			'0' => '⁰',
+			'1' => '¹',
+			'2' => '²',
+			'3' => '³',
+			'4' => '⁴',
+			'5' => '⁵',
+			'6' => '⁶',
+			'7' => '⁷',
+			'8' => '⁸',
+			'9' => '⁹',
+		];
+		$before = mb_substr($string, 0, -4);
+		$places = mb_substr($string, -4, 2);
+		$after = mb_substr($string, -2, 2);
+
+		$new = '';
+		for ($i = 0; $i < 2; $i++) {
+			$new .= $map[mb_substr($places, $i, 1)];
+		}
+		return $before . $new . $after;
 	}
 
 	protected function _locale() {

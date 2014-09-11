a0f59b02138556878999f8cdfb9f2fd6b2b7e329
David Persson <davidpersson@gmx.de>
2014-09-11 14:30:07 +0200

    Fix schema update.

--- data/upgrade/005-inline-tax.sql
+++ data/upgrade/005-inline-tax.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `billing_invoice_positions` ADD `tax_type` VARCHAR(20)   NOT NULL  AFTER `quantity`;
-ALTER TABLE `billing_invoice_positions` ADD `tax_rate` INT(5)  UNSIGNED  NOT NULL  DEFAULT '0'  AFTER `tax`;
+ALTER TABLE `billing_invoice_positions` ADD `tax_rate` INT(5)  UNSIGNED  NOT NULL  DEFAULT '0'  AFTER `tax_type`;
 
 -- Must migrate invoice tax_rate to position tax_rate AND tax_note.
 -- ALTER TABLE `billing_invoices` DROP `tax_note`;

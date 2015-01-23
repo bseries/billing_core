9d3157264668c9c2afba7c62d2ea98e3a73fac71
David Persson <davidpersson@gmx.de>
2015-01-23 12:11:24 +0100

    Add missing update sql for users tax_country.

--- data/upgrade/008-change-user-billing-names.sql
+++ data/upgrade/008-change-user-billing-names.sql
@@ -6,5 +6,6 @@ ALTER TABLE `virtual_users` CHANGE `billing_currency` `currency` CHAR(3)  CHARAC
 ALTER TABLE `virtual_users` CHANGE `billing_vat_reg_no` `vat_reg_no` VARCHAR(100)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL  COMMENT 'billing_core';
 ALTER TABLE `virtual_users` ADD `tax_country` CHAR(2)  NULL  DEFAULT NULL  COMMENT 'billing_core' AFTER `vat_reg_no`;
 
+UPDATE `users` SET `tax_country` = 'DE' WHERE 1 = 1;
 UPDATE `virtual_users` SET `tax_country` = 'DE' WHERE 1 = 1;
 

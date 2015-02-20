8256b8b3c858e0038d710adf33b82d92b580ce16
David Persson <davidpersson@gmx.de>
2015-02-20 12:57:17 +0100

    Fix migration.

--- data/upgrade/010-move-tax-to-invoice.sql
+++ data/upgrade/010-move-tax-to-invoice.sql
@@ -1,3 +1,4 @@
+ALTER TABLE `billing_invoices` ADD `tax_type` VARCHAR(20)  NOT NULL  DEFAULT ''  AFTER `tax_note`;
 ALTER TABLE `billing_invoice_positions` DROP `tax_type`;
 ALTER TABLE `billing_invoice_positions` CHANGE `tax_rate` `amount_rate` INT(5)  UNSIGNED  NOT NULL  DEFAULT '0';
 ALTER TABLE `billing_invoice_positions` MODIFY COLUMN `amount_rate` INT(5) UNSIGNED NOT NULL DEFAULT '0' AFTER `amount_type`;

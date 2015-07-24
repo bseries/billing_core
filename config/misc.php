17dbe61b4f4e934f886f8d007c00ff43403bf223
David Persson <davidpersson@gmx.de>
2015-07-24 19:31:34 +0200

    Bring back removed config.

--- config/misc.php
+++ config/misc.php
@@ -12,8 +12,12 @@
 
 namespace billing_core\config;
 
-use li3_access\security\Access;
 use base_core\security\Gate;
+use billing_core\models\TaxTypes;
+use li3_access\security\Access;
+use lithium\g11n\Message;
+
+extract(Message::aliases());
 
 // Register additional roles.
 Gate::registerRole('merchant');
@@ -26,5 +30,40 @@ Access::add('entity', 'user.role:merchant', function($user, $entity) {
 Access::add('entity', 'user.role:customer', function($user, $entity) {
 	return $user->role == 'customer';
 });
+
+// Tax that applies on all goods when business resides in Germany.
+// B2B & B2C
+TaxTypes::register('DE.vat.standard', [
+	'name' => function($locale) use ($t) {
+		return $t('VAT', ['scope' => 'billing_core', 'locale' => $locale]);
+	},
+	'title' => function($locale) use ($t) {
+		return $t('VAT Standard DE', ['scope' => 'billing_core', 'locale' => $locale]);
+	},
+	'rate' => 19,
+	'note' => $t('Includes 19% VAT.', ['scope' => 'billing_core'])
+]);
+
+// Tax that applies on certain goods when business resides in Germany.
+TaxTypes::register('DE.vat.reduced', [
+	'name' => function($locale) use ($t) {
+		return $t('red. VAT', ['scope' => 'billing_core', 'locale' => $locale]);
+	},
+	'title' => function($locale) use ($t) {
+		return $t('VAT Reduced DE', ['scope' => 'billing_core', 'locale' => $locale]);
+	},
+	'rate' => 7,
+	'note' => $t('Includes 7% VAT.', ['scope' => 'billing_core'])
+]);
+
+// Applies under certain circumstances worldwide.
+TaxTypes::register('*.vat.reverse', [
+	'name' => null,
+	'title' => function($locale) use ($t) {
+		return $t('VAT Reverse Charge', ['scope' => 'billing_core', 'locale' => $locale]);
+	},
+	'rate' => false,
+	'note' => $t('Reverse Charge.', ['scope' => 'billing_core'])
+]);
 
 ?>

fc3605c43e401c8f470bf1eade77670cbc8bb181
David Persson <davidpersson@gmx.de>
2015-07-24 16:18:17 +0200

    More generic SQL comments.

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,9 @@
 -- Augment other tables
-ALTER TABLE `users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billing_core' AFTER `timezone`;
-ALTER TABLE `users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing_core' AFTER `currency`;
-ALTER TABLE `users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing_core' AFTER `vat_reg_no`;
+ALTER TABLE `users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billing' AFTER `timezone`;
+ALTER TABLE `users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing' AFTER `currency`;
+ALTER TABLE `users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing' AFTER `vat_reg_no`;
 
-ALTER TABLE `virtual_users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billing_core' AFTER `timezone`;
-ALTER TABLE `virtual_users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing_core' AFTER `currency`;
-ALTER TABLE `virtual_users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing_core' AFTER `vat_reg_no`;
+ALTER TABLE `virtual_users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billing' AFTER `timezone`;
+ALTER TABLE `virtual_users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing' AFTER `currency`;
+ALTER TABLE `virtual_users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing' AFTER `vat_reg_no`;
 

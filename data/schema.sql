c5096850c06af482d81b320173106a7b6299e30e
David Persson <davidpersson@gmx.de>
2015-07-25 15:46:23 +0200

    Drop vu.

--- data/schema.sql
+++ data/schema.sql
@@ -3,7 +3,3 @@ ALTER TABLE `users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billi
 ALTER TABLE `users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing' AFTER `currency`;
 ALTER TABLE `users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing' AFTER `vat_reg_no`;
 
-ALTER TABLE `virtual_users` ADD `currency` char(3) NOT NULL DEFAULT 'EUR' COMMENT 'billing' AFTER `timezone`;
-ALTER TABLE `virtual_users` ADD `vat_reg_no` varchar(100) DEFAULT NULL COMMENT 'billing' AFTER `currency`;
-ALTER TABLE `virtual_users` ADD `billing_address_id` int(11) unsigned DEFAULT NULL COMMENT 'billing' AFTER `vat_reg_no`;
-
